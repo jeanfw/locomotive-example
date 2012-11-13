7aa633811fc94a2401f43c3ee1a9b765692ddd6a
Jean Friesewinkel <jeanfw@gmail.com>
2012-11-14 00:33:23 +0100

    Corrected DB to use Localhost test.

--- config/environments/development.js
+++ config/environments/development.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 module.exports = function() {
   this.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  this.set('db-uri', 'mongodb://localhost/piitri');
+  this.set('db-uri', 'mongodb://localhost/test');
   this.set('view options', {
     pretty: true
   });
