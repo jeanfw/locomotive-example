b7b49bef8259e60bdd98b3800c4c92389778b209
Jean Friesewinkel <jeanfw@gmail.com>
2012-11-14 17:14:04 +0100

    Added session support to get Passport to work.

--- config/environments/all.js
+++ config/environments/all.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var passport = require('passport');
+var flash = require('connect-flash');
 
 module.exports = function() {
   this.set('views', __dirname + '/../../app/views');
@@ -8,9 +9,17 @@ module.exports = function() {
   this.use(express.logger({ format: '\x1b[1m:method\x1b[0m \x1b[33m:url\x1b[0m :response-time ms' }));
   this.use(express.cookieParser());
   this.use(express.bodyParser());
-
+  
+  // Critical addition! Otherwise req.user isn't set
+  this.use(express.session({ secret: 'locoloco' }));
+  
+  // Initialize Passport
   this.use(passport.initialize());
   this.use(passport.session());
+  
+  // Restore flash message functionality removed from Express 3.x
+  // Uses jaredhanson/connect-flash
+  this.use(flash());
 
   this.use(this.router);
   this.use(express.static(require('path').resolve(__dirname + "/../../public")));
