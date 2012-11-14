4ed424ebe9353d0e9243dbf5d8d990a56c84185f
Jean Friesewinkel <jeanfw@gmail.com>
2012-11-14 10:43:27 +0100

    Correcting jade layout

--- config/initializers/01_mongoose.js
+++ config/initializers/01_mongoose.js
@@ -1,5 +1,4 @@
-module.exports = function(){
-
+module.exports = function() {
 	this.mongoose = require('mongoose');
 	this.mongoose.connect(this.set('db-uri'));
 	this.mongooseTypes = require("mongoose-types");
